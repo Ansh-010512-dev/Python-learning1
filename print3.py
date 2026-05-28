7123fba0203f3ccd8698d3e3222b3ee4ff256f41
Ansh <mrrannsh555.gmail.com>
2026-05-28 19:20:24 +0530

    Add README file with project title

---
+++ print3.py
@@ -0,0 +1,9 @@
+print("print is a function in python 3 for printing output to console.") 
+print("it can take multiple statements and print them in one line")
+example =  "i.e.print('hello everyone')"
+print(example)
+
+                                            # output
+                                            ##print is a function in python 3 for printing output to console.
+               #it can take multiple statements and print them in one line
+                              #i.e.print('hello everyone')
